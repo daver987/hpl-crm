42422cb862ff647699cfc885bd25fffb4818e161
daver987 <daveyalexander@outlook.com>
2023-01-26 10:44:43 -0500

    Chore: clean up code

--- composables/loader.ts
+++
@@ -1,9 +0,0 @@
-import { Loader } from '@googlemaps/js-api-loader'
-
-const loader = new Loader({
-  apiKey: import.meta.env.VITE_MAPS_KEY as string,
-  libraries: ['places'],
-  version: 'weekly',
-  region: 'ca',
-})
-export { loader }
