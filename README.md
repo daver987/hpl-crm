689154822d72e4cfe04531a8389717e31c55321c
daver987 <daveyalexander@outlook.com>
2023-01-30 08:03:05 -0500

    Add: Total Order Value card

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# Nuxt 3 Minimal Starter
+# HPL CRM
 
-Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
-
+A CRM Designed for the high end Limo Service to take advantage of modern tech in a low tech business
 ## Setup
 
 Make sure to install the dependencies:
--- pages/index.vue
+++ pages/index.vue
@@ -24,11 +24,16 @@ const todaysQuotes = quotes?.value?.filter((item) =>
     new Date(format(new Date(), 'yyyy-MM-dd'))
   )
 ).length
+const totalOrdersArray = quotes?.value?.filter((item) => item.isBooked === true)
+let total = totalOrdersArray?.reduce(
+  (sum, trip) => sum + trip.roundTripTotal,
+  0
+)
 </script>
 
 <template>
   <q-page padding>
-    <div class="row q-gutter-md">
+    <div class="row q-gutter-md q-mb-sm">
       <q-card class="col bg-blue">
         <q-card-section horizontal class="row justify-between">
           <q-card-section>
@@ -45,6 +50,41 @@ const todaysQuotes = quotes?.value?.filter((item) =>
           <q-card-section>
             <div class="text-h6">Today's Orders</div>
             <div class="text-h6">{{ orderCount }}</div>
+          </q-card-section>
+          <q-card-section class="flex row justify-end">
+            <q-icon name="request_quote" size="4rem" />
+          </q-card-section>
+        </q-card-section>
+      </q-card>
+      <q-card class="col bg-purple">
+        <q-card-section horizontal class="row justify-between">
+          <q-card-section>
+            <div class="text-h6">Active Quotes</div>
+            <div class="text-h6">{{ quoteCount }}</div>
+          </q-card-section>
+          <q-card-section class="flex row justify-end">
+            <q-icon name="request_quote" size="4rem" />
+          </q-card-section>
+        </q-card-section>
+      </q-card>
+    </div>
+    <div class="row q-gutter-md">
+      <q-card class="col bg-blue">
+        <q-card-section horizontal class="row justify-between">
+          <q-card-section>
+            <div class="text-h6">Today's Quotes</div>
+            <div class="text-h6">{{ todaysQuotes }}</div>
+          </q-card-section>
+          <q-card-section class="flex row justify-end">
+            <q-icon name="request_quote" size="4rem" />
+          </q-card-section>
+        </q-card-section>
+      </q-card>
+      <q-card class="col bg-orange">
+        <q-card-section horizontal class="row justify-between">
+          <q-card-section>
+            <div class="text-h6">Total Orders Value</div>
+            <div class="text-h6">${{ total }}</div>
           </q-card-section>
           <q-card-section class="flex row justify-end">
             <q-icon name="request_quote" size="4rem" />
