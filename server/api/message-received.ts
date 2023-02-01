bee3309cdee1ff310701ace4e7580bb6ba5a29dd
daver987 <daveyalexander@outlook.com>
2023-02-01 04:05:46 -0500

    Add: endpoint for receiving messages

--- server/api/message-received.ts
+++ server/api/message-received.ts
@@ -2,10 +2,21 @@ import { serverSupabaseClient } from '#supabase/server'
 import { Database } from '~/types/supabase'
 
 export default defineEventHandler(async (event) => {
-  const supabase = serverSupabaseClient<Database>(event)
-  const body = readBody(event)
-  const { data, error } = await supabase
-    .from('messages')
-    .insert({ message_json: body })
-  return { success: 201 }
+  try {
+    const supabase = serverSupabaseClient<Database>(event)
+    const query = await getQuery(event)
+    const { SmsMessageSid, SmsStatus, Body, From } = query
+    const { data, error } = await supabase.from('messages').insert([
+      {
+        message_meta: query,
+        message: Body,
+        sender_id: From,
+        message_id: SmsMessageSid,
+        status: SmsStatus,
+      },
+    ])
+    return { success: 201 }
+  } catch (e) {
+    return e
+  }
 })
