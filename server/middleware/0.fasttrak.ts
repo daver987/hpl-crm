8fad9840c71f19e704197522149565ccc1e52ae3
daver987 <david@drobertson.pro>
2023-04-17 08:23:37 -0400

    Test: trying out on Vercel

--- server/middleware/0.fasttrak.ts
+++ server/middleware/0.fasttrak.ts
@@ -1,30 +1,19 @@
+import { getCookie, setCookie } from 'h3'
 import { fasttrakAuth } from '../services/fasttrakInit'
 import type { AuthResponse } from '../services/fasttrakInit'
-import { getToken, setToken, getLastUpdated } from '../services/tokenManager'
-
-declare module 'h3' {
-  interface H3EventContext {
-    auth: AuthResponse
-  }
-}
 
 export default eventHandler(async (event) => {
-  let auth = getToken()
-  const now = new Date()
-  const tokenExpiration = 1800 * 1000 // Token expiration time in milliseconds
+  let accessToken = getCookie(event, 'accessToken')
 
-  if (
-    !auth ||
-    !getLastUpdated() ||
-    now.getTime() - getLastUpdated().getTime() > tokenExpiration
-  ) {
+  if (!accessToken) {
     console.log(
       'Calling authenticateFasttrak from [middleware]',
       new Date().toISOString()
     )
-    auth = await fasttrakAuth()
-    setToken(auth)
+    const authResponse: AuthResponse = await fasttrakAuth()
+    accessToken = authResponse.item.token.accessToken
+    setCookie(event, 'accessToken', accessToken)
   }
 
-  event.context.accessToken = auth.item.token.accessToken
+  event.context.accessToken = accessToken
 })
