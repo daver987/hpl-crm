a1c66102dcdd789fe4c6a1d5456a84bc2c24b53e
daver987 <david@drobertson.pro>
2023-05-17 13:49:14 -0400

    Add: plugin for tel-input

--- server/trpc/trpc.ts
+++ server/trpc/trpc.ts
@@ -1,5 +1,5 @@
 /**
- * This is your entry point to setup the root configuration for tRPC on the server.
+ * This is your entry point to set up the root configuration for tRPC on the server.
  * - `initTRPC` should only be used once per app.
  * - We export only the functionality that we use so we can enforce which base procedures should be used
  *
